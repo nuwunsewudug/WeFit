a96d8dae774e0d96efd12e89b3f2eedcbdbc156f
rod3539 <salix0228@gmail.com>
2022-07-22 11:50:27 +0900

    login시범

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    token: localStorage.getItem('token') || '',
+    currentUser: {},
   },
   getters: {
+    isLoggedIn: state => !!state.token,
+    currentUser: state => state.currentUser,
   },
   mutations: {
+    Login(state) {
+      state.isLogin = true
+    },
+    SET_TOKEN: (state, token) => state.token = token,
+    SET_CURRENT_USER: (state, user) => state.currentUser = user,
   },
   actions: {
+    saveToken({ commit }, token) {
+      commit('SET_TOKEN', token)
+      localStorage.setItem('token')
+    },
+    
+    login({ commit, dispatch }, userData) {
+      axios.post()
+    }
   },
   modules: {
   }
