f4d238fc0fc573fe3e34ad19c996d77706c5e06f
rod3539 <salix0228@gmail.com>
2022-07-24 23:43:24 +0900

    store에 login추가

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,5 +1,6 @@
 import { createStore } from 'vuex'
 import axios from 'axios'
+import router from '@/router'
 
 export default createStore({
   state: {
@@ -9,6 +10,7 @@ export default createStore({
   getters: {
     isLoggedIn: state => !!state.token,
     currentUser: state => state.currentUser,
+    authError: state => state.authError,
   },
   mutations: {
     Login(state) {
@@ -16,6 +18,7 @@ export default createStore({
     },
     SET_TOKEN: (state, token) => state.token = token,
     SET_CURRENT_USER: (state, user) => state.currentUser = user,
+    SET_AUTH_ERROR: (state, error) => state.authError = error
   },
   actions: {
     saveToken({ commit }, token) {
@@ -24,9 +27,36 @@ export default createStore({
     },
     
     login({ commit, dispatch }, userData) {
-      axios.post()
+      axios.post('url', userData)
+      .then(res => {
+        commit('SET_TOKEN', res.data.key)
+        localStorage.setItem('token', res.data.key)
+        commit('LOGIN')
+        dispatch('fetchCurrentUser')
+        router.push({ name: 'urlname' })
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    },
+
+    fetchCurrentUser({ commit, getters, dispatch }) {
+      if (getters.isLoggedIn) {
+        axios({
+          url: drf.accounts.currentUserInfo(),
+          method: 'get',
+          headers: getters.authHeader,
+        })
+          .then(res => commit('SET_CURRENT_USER', res.data))
+          .catch(err => {
+            if (err.response.status === 401) {
+              dispatch('removeToken')
+              router.push({ name: 'LoginView' })
+            }
+          })
+        }
+      },
+    },
+    modules: {
     }
-  },
-  modules: {
-  }
 })
