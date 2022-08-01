436a24906c3c69bd8f8e00559f79b7265f731bcb
rod3539 <salix0228@gmail.com>
2022-08-01 13:40:54 +0900

    main.js수정

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,18 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import dotenv from 'dotenv'
-import gAuthPlugin from 'vue3-google-oauth2'
 
 const app = createApp(App)
 
-dotenv.config()
-
-let gauthClientId = process.env.VUE_APP_API_URL
-app.use(gAuthPlugin, {
-  clientId: gauthClientId,
-  scope: 'email',
-  prompt: 'consent',
-})
   
 app.use(store).use(router).mount('#app')
