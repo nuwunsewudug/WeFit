62e3771535b0875e7b12260df8bf68660450a82c
rod3539 <salix0228@gmail.com>
2022-08-08 13:23:38 +0900

    loginhome router 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import NonLoginView from '@/views/MainPage/NonLoginHome.vue'
 import LoginHome from '@/views/MainPage/LoginHome.vue'
 import LoginView from '@/views/Accounts/LoginView.vue'
 import SignupView from '@/views/Accounts/SignupView.vue'
 
 const routes = [
+  {
+    path: '/',
+    name: 'NonLoginView',
+    component: NonLoginView
+  },
   {
     path: '/Home',
     name: 'LoginHome',
